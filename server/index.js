8d837bc4fc224d47ca2367e6164d0f7725fddcbf
Samson <dubuy.samson85@gmail.com>
2024-01-18 16:24:58 +0100

    [ADD] Design Sprint

---
+++ server/index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+
+dotenv.config();
+
+const app = express();
+
+// Middleware CORS personnalisé
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+// Autres middlewares et routes
+app.use(cors({ origin: 'http://localhost:5173' }));
+app.options('*', cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port 8080'));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
